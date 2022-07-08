f98343e58e1170f6a11467b401909429e36009cf
lucielany <lucielayapura@gmail.com>
2022-07-08 10:57:08 +0200

    separacion de partes y añado funciones

---
+++ mainHome.php
@@ -0,0 +1,26 @@
+<div class="col-9">
+    <!-- Parte central-->
+    <section class="row mas_vendidos">
+        <div class="col-12">
+            <h2>Los más vendidos</h2>
+
+            <div class="vendidos_contenedor">
+                
+                <?php pintaMasVendidos(4);?>
+
+            </div>
+        </div>
+    </section>
+
+    <section class="row novedades">
+        <div class="col-12">
+            <h2>Novedades</h2>
+            <div class="vendidos_contenedor">
+               
+            <?php pintaNovedades(4);?>
+
+            </div>
+        </div>
+    </section>
+</div>
+<!--Fin de la parte central-->
