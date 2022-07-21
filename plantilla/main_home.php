4108635873e259f0d14dd36068d56e16b330891f
lucielany <lucielayapura@gmail.com>
2022-07-21 14:17:06 +0200

    Añado carrusel en main_home

--- plantilla/main_home.php
+++ plantilla/main_home.php
@@ -1,3 +1,27 @@
+<!-- Inicio carrusel -->
+<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="./img/series1.png" class="d-block w-100" alt="series">
+    </div>
+    <div class="carousel-item">
+      <img src="./img/series2.png" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+ <!-- Fin carrusel -->
 <div class="col-12">
     <section class="row mas_vendidos">
         <div class="col-12">
