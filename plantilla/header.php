b15dffb06d94c1b5b1d5d014001d409d581b31c2
lucielany <lucielayapura@gmail.com>
2022-08-02 14:26:18 +0200

    Cambios en header, divido col a parte y  añado botón sesion

--- plantilla/header.php
+++ plantilla/header.php
@@ -2,7 +2,7 @@
     <div class="col-2">
         <img class="logo" src="./img/logo.png" alt="logo" title="logo">
     </div>
-    <div class="col-10">
+    <div class="col-8">
         <ul class="menu_superior">
             <li class="menu_sup_item">
                 <a href="index.php">Inicio</a>
@@ -13,25 +13,25 @@
             <li class="menu_sup_item">
                 <a href="">Opiniones</a>
             </li>
-            <li class="menu_sup_item">
-                <?php
-            if (isset($_SESSION["usuario"])) {
-                echo "<b>Hola, " . $_SESSION["usuario"] . "</b><br>";
-                echo "<a href='logout.php'>Logout</a>";
-             }
-             else{
-                ?>
-                <!-- Button trigger modal -->
-                <a href="login.php"  data-bs-toggle="modal" data-bs-target="#exampleModal">
-                Iniciar Sesión
-                </a>
-               <!-- echo "<a href='login.php'>login</a>";-->
-             <?php }
-             ?>
-            </li>
+
         </ul>
     </div>
-    
+    <div class="col-2 sesion">
+        <?php
+        if (isset($_SESSION["usuario"])) {
+            echo "<b>Hola, " . $_SESSION["usuario"] . "</b><br>";
+            echo "<a href='logout.php'>Logout</a>";
+        } else {
+        ?>
+            <!-- Button trigger modal -->
+            <a href="login.php" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                <button type="button" class="btn btn-outline-primary">Iniciar Sesión
+            </button></a>
+        <?php }
+        ?>
+            
+    </div>
+
 </header>
 
 <?php include("login.php"); ?>
